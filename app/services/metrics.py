4f6f6864685454ae8f4e4b9ce37e1498ce06459c
pranavialla <47995889+pranavialla@users.noreply.github.com>
2025-11-29 19:02:27 +0530

    added session

--- app/services/metrics.py
+++ app/services/metrics.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 
 
 def record_click(short_code: str):
-        db = database.get_db()
+        db = database.SessionLocal()
         try:
                 updated = repository.increment_click(db, short_code)
                 if updated:
